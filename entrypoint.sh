ede1dad02549594a1e4502c45f819a3c398e5586
石渠清心 <Felix2yu@users.noreply.github.com>
2026-06-25 20:37:41 +0800

    修复：entrypoint.sh 添加命令存在性检查

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,12 @@
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
-groupmod -o -g "$PGID" appuser
-usermod  -o -u "$PUID" appuser
+if command -v groupmod >/dev/null 2>&1; then
+    groupmod -o -g "$PGID" appuser
+fi
+if command -v usermod >/dev/null 2>&1; then
+    usermod  -o -u "$PUID" appuser
+fi
 
 chown -R appuser:appuser /app
 
